5503bfb55070596b3f4397dbcfee592d922dffa1
deniscapp <denis_cappelini@hotmail.com>
2016-07-10 18:59:21 -0300

    default scripts relocated

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,8 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
+chrome.tabs.executeScript(null, {file: "scripts/icons.js"});
+
 chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
