7e1f03e1003be7a3e412e6dd16ea150f387fe810
deniscapp <denis_cappelini@hotmail.com>
2016-07-10 19:00:09 -0300

    external content injection

---
+++ app/scripts/external-content.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var materialScript = document.createElement("script");
+materialScript.src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js";
+var linkCSS = document.createElement("link");
+var linkIcons = document.createElement("link");
+var scriptIconsFA = document.createElement("script");
+scriptIconsFA.src = "https://use.fontawesome.com/4587191a7b.js";
+
+linkIcons.rel = "stylesheet";
+linkIcons.href = "http://fonts.googleapis.com/icon?family=Material+Icons";
+
+linkCSS.rel = "stylesheet";
+linkCSS.href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css";
+
+var head = document.getElementsByTagName("head");
+
+document.documentElement.appendChild(linkCSS);
+document.documentElement.appendChild(linkIcons);
+document.documentElement.appendChild(scriptIconsFA);
+// document.documentElement.appendChild(materialScript);
